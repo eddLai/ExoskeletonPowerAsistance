acfa5d16aec0aaeb1bd4ee6af91ece2800f33325
Zhixiang0521 <86817747+Zhixiang0521@users.noreply.github.com>
2024-10-15 01:19:24 +0800

    Put some parameters of the scaled opensim model into subject.xml

--- simulation/CEINMS/ik-to-sto.py
+++ simulation/CEINMS/ik-to-sto.py
@@ -1,31 +1,30 @@
 import opensim as osim
 
-# 加載模型
-model = osim.Model("path_to_your_model.osim")
+path_to_model = "C:/Users/sean9/Desktop/NTK_Cap/Model_Pose2Sim_Halpe26.osim"
+# path_to_scaling_setup_file = ""
+path_to_marker_data = "C:/Users/sean9/Desktop/NTK_Cap/test_data/path1_03/Empty_project_filt_0-30.trc"
+path_to_ik_output = "C:/Users/sean9/Desktop/NTK_Cap/output/ik_output.mot"
+path_to_kinematics_q = "C:/Users/sean9/Desktop/NTK_Cap/output/walk_subject01_Kinematics_q.sto"
 
-# 創建逆向運動學工具並設置參數
-ik_tool = osim.InverseKinematicsTool("path_to_ik_setup_file.xml")
+# scale_tool = osim.ScaleTool()
+# scale_tool.setSetupFileName(path_to_scaling_setup_file)
+# scale_tool.getGenericModelMaker().setModelFileName("path_to_generic_model.osim")
+# scale_tool.getModelScaler().setMarkerFileName("path_to_marker_data.trc")
+# scale_tool.getModelScaler().setOutputModelFileName("scaled_model.osim")
+model = osim.Model(path_to_model)
+ik_tool = osim.InverseKinematicsTool()
 ik_tool.setModel(model)
-ik_tool.setMarkerDataFileName("path_to_marker_data.trc")
-ik_tool.setOutputMotionFileName("ik_output.mot")
+ik_tool.setMarkerDataFileName(path_to_marker_data)
+ik_tool.setOutputMotionFileName(path_to_ik_output)
 ik_tool.run()
-
-# 初始化系統
 state = model.initSystem()
 
-# 創建 Kinematics 分析工具並添加到模型中
 kinematics = osim.Kinematics()
-kinematics.setInDegrees(False)  # 設置為以弧度顯示
+kinematics.setInDegrees(False)
 model.addAnalysis(kinematics)
-
-# 初始化狀態並執行模擬
 state = model.initSystem()
-
-# 創建模擬管理器
 manager = osim.Manager(model)
 manager.initialize(state)
+manager.integrate(10.0)
 
-# 進行模擬並保存結果
-manager.integrate(10.0)  # 模擬 10 秒
-
-kinematics.getPositionStorage().printToXML("walk_subject01_Kinematics_q.sto")
+kinematics.getPositionStorage().printToXML(path_to_kinematics_q)
